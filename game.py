6057bc34070fa896614cac12ada510e3f7abd766
qianlu <qianlu@corp.netease.com>
2018-11-15 17:40:23 +0800

    'Chang-the-data-structure'

--- game.py
+++ game.py
@@ -1,58 +1,102 @@
 # -*- coding:UTF-8 -*-
 class Game(object):
     def __init__(self):
-        # {'white': {1: [4, 7], 2: [0]}, 'black': {}}
-        # self.white = {}
-        # self.black = {}
-        self.chess = {'white': {}, 'black': {}}
+        # self.chess = [[0]*16]*16
+        self.chesses = [ ["." for n in xrange(15)] for m in xrange(15) ]
+        self.top_chessboard = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14']
+        chessboard = self.draw()
+        print chessboard
 
-    def get_type(self, type):
-        return self.chess.get(type)
+    def draw(self):
+        chessboard = '   ' + ' '.join(self.top_chessboard) + '\n'
+        for index, value in enumerate(self.chesses):
+            chessboard += self.top_chessboard[index] + "  "  
+            chessboard += "  ".join(value)
+            if index < 14:
+               chessboard += "\n"
+        return chessboard
+        
+    def next(self, chess_type, row, column):
+        if self.chesses[row][column] == ".":
+            if chess_type == "white":
+                self.chesses[row][column] = "X"
+            else:
+                self.chesses[row][column] = "O"
+        chessboard = self.draw()
+        print chessboard
+        self.check(row, column)
 
-    def next(self, type, row, column):
-        type_value = self.get_type(type)
-        exist = type_value.has_key(row)
-        if exist:
-            type_value[row].append(column)
-        else :
-            type_value[row] = [column]
-        print(self.chess)        
-        self.check(type_value, row, column)
-    
-    def check(self, type_value, row, column):
+    def result(self, count):
+        if count == 5:
+            print("success")
+            return "SUCCESS"
+
+    def check(self, row, column):
         # 横向搜索 - 向左搜索
-        row_count = 0
-        for i in range(0, column):
-            if i in type_value.get(row):
+        row_count = 1
+        for each in range(column - 1, -1, -1):
+            if self.chesses[row][each] == self.chesses[row][column]:
                 row_count += 1
+                self.result(row_count)
+            else:
+                break
         # 横向搜索 - 向右搜索
-        for i in range(column+1, 15):
-            if i in type_value.get(row):
+        for each in range(column + 1, 15):
+            if self.chesses[row][each] == self.chesses[row][column]:
                 row_count += 1
-        print 'row_count', row_count
+                self.result(row_count)
+            else:
+                break
+        print row, column, row_count
         # 纵向搜索 - 向上搜索
-        column_count = 0
-        for i in range(0, row):
-            exist = type_value.get(row, [])
-            if column in exist:
+        column_count = 1
+        for each_row in range(row - 1, -1, -1):
+            if self.chesses[each_row][column] == self.chesses[row][column]:
                 column_count += 1
+                self.result(column_count)
+            else:
+                break
         # 纵向搜索 - 向下搜索
-        for i in range(row, 15):
-            exist = type_value.get(row, [])
-            if column in exist:
+        for each_row in range(row + 1, 15):
+            if self.chesses[each_row][column] == self.chesses[row][column]:
                 column_count += 1
-        print 'column_count', column_count
+                self.result(column_count)
+            else:
+                break
+        # 斜向搜索 - 左上搜索
+        left_incline_count = 1
+        for each in zip(range(row - 1, -1, -1), range(column - 1, -1, -1)):
+            each_row = each[0]
+            each_column = each[1]
+            if self.chesses[each_row][each_column] == self.chesses[row][column]:
+                left_incline_count += 1
+                self.result(left_incline_count)
+            else:
+                break
+        # 斜向搜索 - 右下搜索
+        for each in zip(range(row + 1, 15), range(column + 1, 15)):
+            each_row = each[0]
+            each_column = each[1]
+            if self.chesses[each_row][each_column] == self.chesses[row][column]:
+                left_incline_count += 1
+                self.result(left_incline_count)
+            else:
+                break
                 
 
-
+    def compare(self, before_row, before_column, row, column, count):
+        pass
 if __name__ == "__main__":
     game = Game()
-    game.next('white', 0, 4)
-    game.next('white', 1, 4)
-    game.next('white', 3, 4)
-    game.next('white', 2, 7)
-    # game.next('black', 2, 6)
-           
-
-    # def check(self, type, site):
-    #     for i in range()
+    game.next('white', 6, 7)
+    game.next('white', 6, 6)
+    game.next('white', 6, 8)
+    game.next('white', 6, 2)
+    game.next('white', 7, 7)
+    game.next('white', 7, 10)
+    game.next('white', 5, 7)
+    game.next('white', 8, 7)
+    # game.next('white', 9, 7)
+    game.next('white', 7, 9)
+    game.next('white', 4, 6)
+    game.next('white', 3, 5)
